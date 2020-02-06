4413bea64633fb98caa5ab7278b111f43745f69f
kobyunghwa <bhko0524@naver.com>
2020-02-06 16:52:54 +0900

    #4 crawling

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Store, Category, Univ
+
+admin.site.register(Univ)
+admin.site.register(Category)
+admin.site.register(Store)
--- core/models.py
+++ core/models.py
@@ -1,16 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# class Category(models.Model):
-#     name = models.CharField()
+
+
+class Univ(models.Model):
+    name = models.CharField(max_length=255)
+    addr = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='카테고리명')
+    univ = models.ForeignKey(Univ, on_delete=models.CASCADE, verbose_name='대학', related_name='category')
+
+    def __str__(self):
+        return self.name
 
 
 class Store(models.Model):
     logo = models.URLField()
-    title = models.CharField(max_length=255)
-    star = models.CharField(max_length=255)
-    min_price = models.PositiveIntegerField()
-    del_time = models.PositiveIntegerField()
+    title = models.CharField(max_length=255, verbose_name='가게명')
+    star = models.CharField(max_length=255, verbose_name='별점')
+    min_price = models.PositiveIntegerField(verbose_name='최소주문금액')
+    del_time = models.PositiveIntegerField(verbose_name='배달시간')
     store_url = models.URLField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='카테고리명', related_name='store')
 
     def __str__(self):
         return self.title
