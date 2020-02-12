9f458202fe5480392002bd02f3b156a448afcdeb
vandlaw7 <vandlaw@naver.com>
2020-02-12 14:23:25 +0900

    safdsfa

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from import_export.admin import ImportExportMixin, ImportMixin
 # Register your models here.
 from core.models import *
 
-class BoardAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+# class BoardAdmin(ImportExportMixin, admin.ModelAdmin):
+#     pass
 
 admin.site.register(Category)
 admin.site.register(Store)
 admin.site.register(Posting)
 admin.site.register(Tag)
-admin.site.register(Boards, BoardAdmin)
+# admin.site.register(Boards, BoardAdmin)
