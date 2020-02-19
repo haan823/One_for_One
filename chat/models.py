144800d7ce02a35cf5b28b713b306a8594f708b6
yejin <cdjin6911@gmail.com>
2020-02-19 16:04:44 +0900

    #수정중 2차

--- chat/models.py
+++ chat/models.py
@@ -27,7 +27,7 @@ class Contact(models.Model):
 
 
 class Message(models.Model):
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room_id = models.PositiveIntegerField()
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
